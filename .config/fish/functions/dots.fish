5c2ac17a38a107253a281f3cfe3f6d87feca63f8
Alex <alex@alexday.me>
2020-03-11 21:17:36 -0400

    Moved git repo

--- .config/fish/functions/dots.fish
+++ .config/fish/functions/dots.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function dots --wraps='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' --description 'alias dots=/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv;
+function dots --wraps='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' --wraps='/usr/bin/git --git-dir=$HOME/.config/.dotfiles/ --work-tree=$HOME' --description 'alias dots=/usr/bin/git --git-dir=$HOME/.config/.dotfiles/ --work-tree=$HOME'
+  /usr/bin/git --git-dir=$HOME/.config/.dotfiles/ --work-tree=$HOME $argv;
 end
