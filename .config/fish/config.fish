8606047393a397def9acce0909400284890a1056
Alex Day <alex@alexday.me>
2020-04-03 19:40:42 -0400

    some weird path setting was going on when trying to be fancy so it is now hardcoded

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 # Profile file. Runs on login. Environmental variables are set here.
 
 # Adds `~/.local/bin` and the rust binaries to $PATH
-export PATH="$PATH:$HOME/.cargo/bin:"(du "$HOME/.local/bin/" | cut -f2 | tr '\n' ':' | sed 's/:*\$//')
+# export PATH="$PATH:$HOME/.cargo/bin:"(du "$HOME/.local/bin/" | cut -f2 | tr '\n' ':' | sed 's/:*\$//')
+export PATH="$PATH:$HOME/.local/bin/statusbar:$HOME/.local/bin/cron:$HOME/.local/bin/"
 
 # Default programs:
 export EDITOR="nvim"
