939c626612c3121ca92498dc6e1e2f5001b791b5
Alex Day <alexday135@gmail.com>
2020-06-09 23:02:43 -0400

    Hide the clunky indicator. We have cursor change

---
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -0,0 +1,5 @@
+# Defined in /tmp/fish.jUj2p0/fish_mode_prompt.fish @ line 2
+function fish_mode_prompt --description 'Displays the current mode'
+    # To reuse the mode indicator use this function instead
+    # fish_default_mode_prompt
+end
